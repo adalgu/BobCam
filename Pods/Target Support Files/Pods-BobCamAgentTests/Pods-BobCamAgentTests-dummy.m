1af482c24b61c01bb97509810c6c20e63fcbd8cf
adalgu <adalgu@gmail.com>
2025-12-21 12:38:23 +0900

    refactor: iOS-First project structure reorganization
    BREAKING CHANGE: Project structure completely reorganized
    
    ## iOS Project (now at root)
    - BobCamAgent.xcodeproj, xcworkspace, Podfile at root level
    - BobCamAgent/ source code directly accessible
    - BobCamAgentTests/, BobCamAgentUITests/ at root
    
    ## Python/Backend (moved)
    - backend/ - Active Python scripts (main_integrated.py, etc)
    - legacy/ - Old versions and deprecated code
    - tests/ - Python test files
    
    ## Removed
    - BobCam-iOS/ wrapper folder (was causing 3-level nesting)
    
    Build verified: pod install + xcodebuild succeeded

---
+++ Pods/Target Support Files/Pods-BobCamAgentTests/Pods-BobCamAgentTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BobCamAgentTests : NSObject
+@end
+@implementation PodsDummy_Pods_BobCamAgentTests
+@end
---
+++ legacy/ios-wrapper/Pods/Target Support Files/Pods-BobCamAgent-BobCamAgentUITests/Pods-BobCamAgent-BobCamAgentUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BobCamAgent_BobCamAgentUITests : NSObject
+@end
+@implementation PodsDummy_Pods_BobCamAgent_BobCamAgentUITests
+@end
